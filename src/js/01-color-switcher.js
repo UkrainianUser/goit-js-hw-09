ae7482984465144a67bef7a8f740da83432dba42
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2023-02-26 15:42:39 +0200

    add  timerId

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,6 +5,7 @@ const body = document.querySelector('body');
 function getRandomHexColor() {
 	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+let timerId;
 
 buttonStart.addEventListener('click', startChangeBgColor);
 buttonStop.addEventListener('click', stopChangeBgColor);
